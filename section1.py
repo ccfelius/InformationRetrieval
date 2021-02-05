aeb4f5d593884cb4f632c6080762551929c4b492
SerahJane <sjvanels@gmail.com>
2021-02-05 14:11:29 +0100

    removed all the if-statements in 1&2

--- section1.py
+++ section1.py
@@ -6,36 +6,34 @@ def read_cacm_docs(root_folder = "./datasets/"):
             appending the title and abstract (separated by a "\n"). 
             In case the record doesn't have an abstract, the document is composed only by the title
     """
-    if os.path.isfile(root_folder+"cacm.all"):
-        f = open(root_folder+"cacm.all", "r")
-        entries = [] 
-        document = ""
-        doc_id = ""
-        doc = ()
-        for line in f:
-            if line.startswith(".I"):
-                doc_id = line.strip(" .I\n")
-            elif line.startswith(".T"):                            
-               
-                # skip to the next line 
-                document = next(f).strip()    
-                following = ""
-                
-                # process the title and abstract with seperate requirements 
-                while not(following.startswith(".B")):
-                    if ".W" in document:
-                        following = following.replace("\n", " ")
-                    document += " "+ following
-                    following = next(f)  
-                document = document.replace(".W", "").replace("\n \n"," ").strip()
+    f = open(root_folder+"cacm.all", "r")
+    entries = [] 
+    document = ""
+    doc_id = ""
+    doc = ()
+    for line in f:
+        if line.startswith(".I"):
+            doc_id = line.strip(" .I\n")
+        elif line.startswith(".T"):                            
             
-            # if unique, add tuple to list 
-            elif doc and doc not in entries: 
-                entries.append(tuple((doc_id, document)))
-            doc = tuple((doc_id, document))  
-        f.close()
-        return entries 
-    raise NotImplementedError()
+            # skip to the next line 
+            document = next(f).strip()    
+            following = ""
+            
+            # process the title and abstract with seperate requirements 
+            while not(following.startswith(".B")):
+                if ".W" in document:
+                    following = following.replace("\n", " ")
+                document += " "+ following
+                following = next(f)  
+            document = document.replace(".W", "").replace("\n \n"," ").strip()
+        
+        # if unique, add tuple to list 
+        elif doc and doc not in entries: 
+            entries.append(tuple((doc_id, document)))
+        doc = tuple((doc_id, document))  
+    f.close()
+    return entries 
 
 # TODO: Implement this! (3 points)
 def read_queries(root_folder = "./datasets/"):
@@ -43,34 +41,32 @@ def read_queries(root_folder = "./datasets/"):
         Reads in the CACM queries. The dataset is assumed to be in the folder "./datasets/" by default
         Returns: A list of 2-tuples: (query_id, query)
     """
-    if os.path.isdir(root_folder):
-        f = open(root_folder+"query.text", "r")
-        queries = [] 
-        query = ""
-        query_id = ""
-        doc = ()
-        for line in f:
-            if line.startswith(".I"):
-                query_id = line.strip(" .I\n")
-            elif line.startswith(".W"):
+    f = open(root_folder+"query.text", "r")
+    queries = [] 
+    query = ""
+    query_id = ""
+    doc = ()
+    for line in f:
+        if line.startswith(".I"):
+            query_id = line.strip(" .I\n")
+        elif line.startswith(".W"):
 
-                # skip to the next line 
-                query = next(f).strip()    
-                following = ""
+            # skip to the next line 
+            query = next(f).strip()    
+            following = ""
 
-                # check for long queries 
-                while not(following.startswith(".N")):
-                    query += " "+ following.strip()
-                    following = next(f)    
-                query.strip()
+            # check for long queries 
+            while not(following.startswith(".N")):
+                query += " "+ following.strip()
+                following = next(f)    
+            query.strip()
 
-            # add tuple to list if unique 
-            elif doc and doc not in queries: 
-                queries.append(tuple((query_id, query)))
-            doc = tuple((query_id, query))  
-        f.close()
-        return queries 
-    raise NotImplementedError()
+        # add tuple to list if unique 
+        elif doc and doc not in queries: 
+            queries.append(tuple((query_id, query)))
+        doc = tuple((query_id, query))  
+    f.close()
+    return queries 
 
 # TODO: Implement this! (1 point)
 def load_stopwords(root_folder = "./datasets/"):
@@ -78,15 +74,13 @@ def load_stopwords(root_folder = "./datasets/"):
         Loads the stopwords. The dataset is assumed to be in the folder "./datasets/" by default
         Output: A set of stopwords
     """
-    if os.path.isdir(root_folder):
-        f = open(root_folder+"common_words", "r")
-        common_words = set()
-        for word in f: 
-            word = word.strip().lower()
-            common_words.add(word)   
-        f.close()
-        return common_words 
-    raise NotImplementedError()
+    f = open(root_folder+"common_words", "r")
+    common_words = set()
+    for word in f: 
+        word = word.strip().lower()
+        common_words.add(word)   
+    f.close()
+    return common_words 
 
 # TODO: Implement this! (4 points)
 def tokenize(text):
@@ -95,11 +89,8 @@ def tokenize(text):
         Input: text - a string
         Output: a list of tokens
     """ 
-  
-    if isinstance(text, str): 
-        tokens = nltk.WordPunctTokenizer().tokenize(text)
-        return tokens
-    raise NotImplementedError()
+    tokens = nltk.WordPunctTokenizer().tokenize(text)
+    return tokens
 
 # TODO: Implement this! (3 points)
 def stem_token(token):
@@ -108,9 +99,6 @@ def stem_token(token):
         Input: a single token
         Output: the stem of the token
     """
-    if token:
-        ps = nltk.PorterStemmer() 
-        stem_token = ps.stem(token)
-        return stem_token
-    raise NotImplementedError()
-    
+    ps = nltk.PorterStemmer() 
+    stem_token = ps.stem(token)
+    return stem_token
--- section2.py
+++ section2.py
@@ -6,25 +6,25 @@ def build_tf_index(documents):
         Input: a list of documents - (doc_id, tokens) 
         Output: An inverted index. [token] -> [(doc_id, token_count)]
     """
-    if isinstance(documents, list):
-        hashtable = dict()
-        inv_index = dict()
-        
-        for document in documents:
-            doc_id = int(document[0])
-            for token in document[1]: 
-                
-                # create element for dictionary 
-                if token not in hashtable:
-                    hashtable[token] = list() 
-                    count = 0 
-                count += 1 
+    hashtable = dict()
+    inv_index = dict()
+    
+    for document in documents:
+        doc_id = int(document[0])
+        for token in document[1]: 
+            
+            # create element for dictionary 
+            if token not in hashtable:
+                hashtable[token] = list() 
+                count = 0 
+            count += 1 
 
-                # create an object and add to the list 
-                tk = tuple((doc_id, count))
-                hashtable[token].append(tk)
+            # create an object and add to the list 
+            tk = tuple((doc_id, count))
+            hashtable[token].append(tk)
 
-                # add list as a key value of the word 
-                inv_index[token] = hashtable[token]
-        return inv_index
-    raise NotImplementedError()
+            # add list as a key value of the word 
+            inv_index[token] = hashtable[token]
+    return inv_index
+
+
